341654a9b9f96b16bcfb79eaac378d3b4d49c06a
Anand Simmy <anandsimmy7@gmail.com>
2020-05-08 21:35:50 +0530

    multiplayer support added

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {  useState, useEffect } from'react'
 import Board from './Board'
 import { connect } from 'react-redux'
@@ -5,20 +6,28 @@ import { calculateWinner } from '../calculateWinner'
 import { confetti } from '../confetti'
 import styles from './styles'
 import { withStyles, Button }  from '@material-ui/core'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import Slide from '@material-ui/core/Slide';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import firebase from 'firebase'
 
 const Game=(props) => {
 
     const { token }= props.match.params
+    const [snackBarOpen, setSnackBarOpen]= useState(true)
+    const [newGameStarted, setNewGameStarted]= useState(false)
+    const [loader, setLoader]=useState(false)
     const [gameData, setGameData]= useState({
         boardState: Array(9).fill(null),
         creatorToken: null,
         joineeToken: null,
-        gameInfo: null,
         xIsNext: true,
         gamers: []
     })
-    const { boardState, xIsNext }= gameData
+    const { boardState, xIsNext, joineeName, creatorName, joineeToken, creatorToken  }= gameData
     const { winner, winningSquares }= calculateWinner(boardState)
     useEffect(()=>{
         firebase
@@ -26,9 +35,9 @@ const Game=(props) => {
             .collection('games')
             .where('gamers','array-contains',token)
             .onSnapshot(res=>{
-                const gameData= res.docs[0].data()
-                console.log(gameData)
+                const gameData= res && res.docs && res.docs[0].data()
                 setGameData(gameData)
+                gameData && setNewGameStarted(gameData.newGameStarted)
             })
     }, [])
 
@@ -36,47 +45,152 @@ const Game=(props) => {
         //if there is already a winner or square is already clicked and have a value inside it, or player not
         //eligible is clicking squares dont do anything
         if(winner || boardState[index] || (xIsNext && gameData.joineeToken === token) || 
-            (!xIsNext && gameData.creatorToken === token))
+            (!xIsNext && gameData.creatorToken === token) || joineeName === 'noname')
             return;
         xIsNext ? boardState[index] = 'X' : boardState[index] = 'O'
         firebase
             .firestore()
             .collection('games')
             .doc(`${gameData.creatorToken}:${gameData.joineeToken}`)
-            .set({
-                ...gameData,
+            .update({
                 boardState: boardState,
                 xIsNext: !xIsNext
             })
     }
-    const resetGame= () => {
-        firebase
+
+    const checkAllNull=(arr) => {
+        //checking all elements are null or not
+        const allNull= arr.every((v) => { return v === null; })
+        return allNull
+    }
+
+    const newGameStartedMessageCloseToDataBase=async() => {
+        setLoader(true)
+        await firebase
             .firestore()
             .collection('games')
             .doc(`${gameData.creatorToken}:${gameData.joineeToken}`)
-            .set({
-                ...gameData,
-                boardState: Array(9).fill(null),
-                xIsNext: true
+            .update({
+                newGameStarted: false
             })
+        setLoader(false)
     }
 
+    const resetGame=async () => {
+        await firebase
+            .firestore()
+            .collection('games')
+            .doc(`${gameData.creatorToken}:${gameData.joineeToken}`)
+            .update({
+                boardState: Array(9).fill(null),
+                xIsNext: true,
+                newGameStarted: true
+            })
+        debugger
+    }
+
+    const titleText=() => {
+        let text= 'WAITING FOR YOUR FRIEND'
+        if(joineeName === 'noname')
+            return text
+        //checking whose name should be displayed for next turn
+        if((xIsNext && (token === creatorToken)) || (!xIsNext && ( token === joineeToken)))
+            text = `YOUR TURN: ${xIsNext?'X':'O'}`
+        else if(xIsNext && (token !== creatorToken) && creatorName)
+            text = `${creatorName.toUpperCase()}'s TURN: ${xIsNext?'X':'O'}`
+        else if(!xIsNext && (token !== joineeToken) && joineeName)
+            text = `${joineeName.toUpperCase()}'s TURN: ${xIsNext?'X':'O'}`
+        return text
+    }
+
+    const winnerText=(winnerIcon) => {
+        if(!boardState.includes(null) && !winnerIcon){
+            return 'GAME TIED!'
+        }
+        const winner= winnerIcon === 'X' ? creatorName : joineeName
+        const winnerText = winnerIcon ? `${winner.toUpperCase()} WON!` : null
+        return winnerText
+    }
+
+    const snackBarMessage=()=> {
+        const message= creatorToken === token ? 
+            `${joineeName && joineeName.toUpperCase()} JOINED THE GAME!` 
+                : `YOU JOINED ${creatorName && creatorName.toUpperCase()}'s GAME!`
+        return message
+    }
+
+    const SlideTransition=(props) => {
+        return <Slide {...props} direction="down" />;
+      }
     const { classes }=  props
     winner ? confetti.start() : confetti.stop()
     
     return (
         <div className={classes.container}>
-            { gameData &&
+            {        
+                loader ?
+                    <Backdrop className={classes.backdrop} open={true}>
+                        <CircularProgress color="inherit" />
+                    </Backdrop>  
+                :
                 <React.Fragment>
-                <div>friend id is: {props.friendToken}</div>
-                <h2 className={classes.header}>
-                    { winner ? 'Winner: ' + winner  : 
-                        'Next is: ' + ( gameData && gameData.xIsNext ? 'X' : 'O' ) }
-                </h2>
-                <Button onClick={resetGame} variant="outlined" color="secondary" className={classes.newGameButton}>
-                    Start New Game
-                </Button>
-                <Board squares={boardState} winningSquares={winningSquares} clickHandler={clickHandler}/>
+                    <h3 className={classes.header}>
+                        {   //doing creatorName check here to avoid 'waiting for friend to join' showing, before backend data for 'shareDiv' loads
+                            winnerText(winner) ? winnerText(winner) : creatorName && titleText() 
+                        }       
+                    </h3>
+                    {   //snackbar for freind joined
+                        joineeName && creatorName && joineeName !== 'noname' && checkAllNull(boardState)  ? 
+                       <Snackbar
+                            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                            autoHideDuration={6000}
+                            open={snackBarOpen}
+                            TransitionComponent={SlideTransition}
+                            onClose={()=>setSnackBarOpen(false)}
+                        >
+                            <MuiAlert elevation={6} variant="filled" severity="success">
+                                {snackBarMessage()}
+                            </MuiAlert>
+                        </Snackbar>
+                       : null
+
+                    }
+                   {    //snackbar for new game started ie, someone clicked on start new game
+                        <Snackbar
+                            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                            autoHideDuration={6000}
+                            open={newGameStarted}
+                            onClose={newGameStartedMessageCloseToDataBase}
+                        >
+                            <MuiAlert elevation={6} variant="filled" severity="success">
+                                NEW GAME STARTED
+                            </MuiAlert>
+                        </Snackbar>
+                    }
+                    {   //show this button only if game got finished either by tie or having a winner
+                        joineeName && joineeName === 'noname' ? 
+                            <div className={classes.shareDiv}>
+                                <div className={classes.shareText}>GAME ID:
+                                    <span className={classes.gameID}>  {joineeToken}</span>
+                                </div>
+                                <div>SHARE WITH YOUR FRIEND</div>
+                            </div>
+                            : 
+                        (joineeName !== undefined && (!boardState.includes(null) || winner )  ? 
+                        <Button onClick={resetGame} variant="outlined" color="secondary" className={classes.newGameButton}>
+                            Start New Game
+                        </Button>  :  null )
+                    }
+                    {   //doing check to avoid loading this block (board and exit icon) before firebase data loads
+                        creatorName && joineeToken ?
+                        <React.Fragment>
+                            <Board squares={boardState} winningSquares={winningSquares} clickHandler={clickHandler}/>
+                            <ExitToAppIcon className={classes.exitIcon} onClick={()=>props.history.push('/')}/>
+                        </React.Fragment>  :
+                        <Backdrop className={classes.backdrop} open={true}>
+                            <CircularProgress color="inherit" />
+                        </Backdrop> 
+                    } 
                 </React.Fragment>
             }
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,66 +1,167 @@
 import React, { useState } from 'react'
-import { withStyles, Paper, FormControl,Input, Button } from '@material-ui/core'
+import { withStyles, FormControl, Button, TextField } from '@material-ui/core'
 import generate from 'meaningful-string'
 import { connect } from 'react-redux'
 import styles from './styles'
+import '../fonts.css'
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import firebase from 'firebase'
 
 const Home=(props) => {
 
     const { classes }= props
-    const [joinGameToken, setJoinGameToken]= useState(null)
+    const [gameCollectionInfo, setGameCollectionInfo]= useState({ state: false})
+    const [errorMessage, setErrorMessage]= useState(null)
+    const [loader, setLoader]=useState(false)
+    const [gamerInfo, setGamerInfo]= useState({})
+    const [joinTokenInfo, setJoinTokenInfo]= useState({})
+    // const [joinGameToken, setJoinGameToken]= useState(null)
 
     const createNewGame= async() => {
+        setLoader(true)
         const options= {
-            min: 6,
-            max: 6,
+            min: 5,
+            max: 5,
             capsWithNumbers: true
         }
         const newGame={
             p1_token: generate.random(options),
             p2_token: generate.random(options)
-        } 
+        }
         await firebase.firestore()
             .collection('games')
             .doc(`${newGame.p1_token}:${newGame.p2_token}`)
             .set({
                 boardState: Array(9).fill(null),
                 xIsNext: true,
-                gameInfo: newGame,
                 creatorToken: newGame.p1_token,
+                creatorName: gamerInfo.name,
+                joineeName: 'noname',
                 joineeToken: newGame.p2_token,
-                gamers:[newGame.p1_token, newGame.p2_token]
+                gamers:[newGame.p1_token, newGame.p2_token],
+                newGameStarted: false
             })
-        .then(()=>{
-            props.dispatchTokens({
-                creatorToken: newGame.p1_token,
-                joineeToken: newGame.p2_token
-            })
-            props.history.push(`/game/${newGame.p1_token}`)
-        })
-        .catch((err)=>{
-            console.log(err)
+            .then(()=>{
+                props.history.push(`/game/${newGame.p1_token}`)
         })
     }
 
-    const joinGame=() => {
-        props.history.push(`/${joinGameToken}`)
+    const joinGame= async() => {
+        setLoader(true)
+        await firebase
+            .firestore()
+            .collection('games')
+            .where('gamers','array-contains', joinTokenInfo.joineeToken)
+            .onSnapshot(async (res) => {
+                const dataArray= res.docs.map(doc => doc.data())
+                if(dataArray.length === 0){
+                    setLoader(false)
+                    return setErrorMessage('Invalid token, please try again')
+                }
+                await firebase
+                    .firestore()
+                    .collection('games')
+                    .doc(`${dataArray[0].creatorToken}:${dataArray[0].joineeToken}`)
+                    .set({
+                        ...dataArray[0],
+                        joineeName: gamerInfo.name
+                    })
+                    .catch((err)=>{
+                        setLoader(false)
+                        console.log(err)
+                    })
+                    props.history.push(`/game/${joinTokenInfo.joineeToken}`)
+            })
+        
     }
+
+    const handleChange=(event) => {
+        setErrorMessage(null)
+        const { name, value }= event.target
+        setGamerInfo({
+            [name]: value
+        })
+    }
+
+    const handleIdChange=(event) =>{
+        setErrorMessage(null)
+        const { name, value }= event.target
+        setJoinTokenInfo({
+            [name]: value
+        })
+    }
+
+    const handleSubmit=(e) => {
+        e.preventDefault()
+        gameCollectionInfo.type === 'creator' ? createNewGame() : joinGame()
+    }
+
+    const resetHomePage=() => {
+        setGameCollectionInfo({state:false})
+        setErrorMessage(null)
+    }
+
+    const infoCollection= (
+        <React.Fragment>
+            <form onSubmit={handleSubmit}>
+                <FormControl className={classes.input}>
+                    <TextField autoFocus name='name' onChange={handleChange}  label='Enter your Name' variant="outlined" color="secondary" required/>
+                </FormControl>
+                { gameCollectionInfo.type === 'joinee' ?  
+                <FormControl className={classes.input}>
+                    <TextField name='joineeToken' onChange={handleIdChange} label='Enter Game ID' variant="outlined" color="secondary"
+                        helperText="*Enter id shared by your friend"required/>
+                </FormControl> : null }
+                <Button type='submit' className={classes.startGameButton} variant='outlined' color='secondary' >
+                    { gameCollectionInfo.type  === 'creator' ? 'Start Game' : 'Join Game' }
+                </Button>
+                {
+                    errorMessage ? <div><span className={classes.error}>{errorMessage}</span></div> : null
+                }
+                <Button className={classes.cancelGameButton} variant='outlined' color='secondary' onClick={resetHomePage}>
+                    Cancel
+                </Button>
+            </form>
+        </React.Fragment>
+    )
 
     return (
-        <div className={classes.homeContainer}>
-            <Paper className={classes.paper}> 
-                <Button variant='outlined' color='secondary' onClick={createNewGame}>
-                    Create New Game
-                </Button>
-                <FormControl className={classes.inputContainer}>
-                    <Input value={joinGameToken} onChange={(e)=>setJoinGameToken(e.target.value)} placeholder='Enter Game ID' />
-                </FormControl>
-                <Button clasName={classes.inputContainer} variant='outlined' color='secondary' onClick={joinGame}>
-                    Join Friend's Game
-                </Button>
-            </Paper>
-        </div>
+        <React.Fragment>
+            { 
+                loader ?
+                <Backdrop className={classes.backdrop} open={ loader } >
+                    <CircularProgress color="inherit" />
+                </Backdrop>  :  
+                <div className={classes.homeContainer}>
+                    <div className={classes.paper}> 
+                    { gameCollectionInfo.state  ?  
+                            infoCollection  : 
+                            <React.Fragment>
+                            <h1 style={{fontFamily: 'Permanent Marker'}} className={classes.mainHeader}>TIC TAC TOE</h1>
+                            <Button className={classes.buttonContainer} variant='outlined' color='secondary' 
+                                onClick={()=> setGameCollectionInfo({
+                                    state: true, 
+                                    type: 'creator'
+                                    })
+                                }>
+                                Create New Game
+                            </Button>
+                            <Button className={classes.buttonContainer} variant='outlined' color='secondary' 
+                                onClick={()=> setGameCollectionInfo({
+                                    state: true, 
+                                    type: 'joinee'
+                                    })
+                                }>
+                                Join Friend's Game
+                            </Button>
+                            </React.Fragment> 
+                        }
+                        </div>
+                </div>
+            } 
+        </React.Fragment>
+        
     )
 }
 
